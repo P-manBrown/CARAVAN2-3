4de5e6386c7a4270bd1df215035a03fa5cf8ca06
P-manBrown <tachikawa.yuya@outlook.jp>
2021-11-19 00:58:07 +0000

    [Finish] 2-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,25 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blog_path
   end
 
   def edit
   end
+
+
+
+
+
+private
+  def blog_params
+    params.reqire(:blog).permit(:title, :category, :body)
+  end
+
 end
